22af95e5df115862b84189337c4987504b5a6bb1
Bostangiu Luca <lucabostangiu@gmail.com>
2025-04-14 11:40:25 +0300

    wqqwqw:wq
    Signed-off-by: Bostangiu Luca <lucabostangiu@gmail.com>

--- labs/lab-06/tasks/courses/support/courses.asm
+++ labs/lab-06/tasks/courses/support/courses.asm
@@ -5,78 +5,114 @@
 extern printf
 
 section .bss:
-	; the structure for a student
-	struc student_t
-		name:   resb	10	; char[10] - student name
-		id_course:	resd	1	; integer - the id of the course where a student is assigned
-		check:	resd	1	; "bool" - check if the student is assigned to any course
-	endstruc
+    ; the structure for a student
+    struc student_t
+        name:   resb	10	; char[10] - student name
+        id_course:	resd	1	; integer - the id of the course where a student is assigned
+        check:	resd	1	; "bool" - check if the student is assigned to any course
+    endstruc
 
     ; the structure for a course
-	struc course_t
-		id:	resd	1	; id = index in courses (the list of courses)
-		name_course:   resb	15	; char[10] - the name of the course
-	endstruc
+    struc course_t
+        id:	resd	1	; id = index in courses (the list of courses)
+        name_course:   resb	15	; char[10] - the name of the course
+    endstruc
 
 section .data
     unassigned:		db "Student unassigned :(", 0
-	v_students_count:    dd 5
+    v_students_count:    dd 5
     v_courses_count:    dd 3
 
     students:
-		istruc student_t
-			at name,	db "Vlad", 0
-			at id_course,		dd 0
-			at check,	dd 1
-		iend
+        istruc student_t
+            at name,	db "Vlad", 0
+            at id_course,		dd 0
+            at check,	dd 1
+        iend
 
-		istruc student_t
-			at name,	db "Andrew", 0
-			at id_course,		dd 1
-			at check,	dd 1
-		iend
+        istruc student_t
+            at name,	db "Andrew", 0
+            at id_course,		dd 1
+            at check,	dd 1
+        iend
 
-		istruc student_t
-			at name,	db "Kim", 0
-			at id_course,		dd 1
-			at check,	dd 1
-		iend
+        istruc student_t
+            at name,	db "Kim", 0
+            at id_course,		dd 1
+            at check,	dd 1
+        iend
 
-		istruc student_t
-			at name,	db "George", 0
-			at id_course,		dd 2
-			at check,	dd 1
-		iend
+        istruc student_t
+            at name,	db "George", 0
+            at id_course,		dd 2
+            at check,	dd 1
+        iend
 
-		istruc student_t
-			at name,	db "Kate", 0
-			at id_course,		dd 0
-			at check,	dd 0
-		iend
+        istruc student_t
+            at name,	db "Kate", 0
+            at id_course,		dd 0
+            at check,	dd 0
+        iend
 
     courses:
-		istruc course_t
-			at id,	dd 0
-			at name_course,	db "Assembly", 0
-		iend
+        istruc course_t
+            at id,	dd 0
+            at name_course,	db "Assembly", 0
+        iend
 
-		istruc course_t
-			at id,	dd 1
-			at name_course,	db "Linear Algebra", 0
-		iend
+        istruc course_t
+            at id,	dd 1
+            at name_course,	db "Linear Algebra", 0
+        iend
 
-		istruc course_t
-			at id,   dd 2
-			at name_course,	db "Physics", 0
-		iend
+        istruc course_t
+            at id,   dd 2
+            at name_course,	db "Physics", 0
+        iend
 
 section .text
 global main
 
 main:
-	push ebp
-	mov ebp, esp
-	PRINTF32 `The students list is:\n\x0`
-	; TODO: Print the list of students and the courses where they are assigned
+    push ebp
+    mov ebp, esp
+    PRINTF32 `The students list is:\n\x0`
+
+    ; Initialize loop counter and pointers
+    mov ecx, [v_students_count]    ; Number of students
+    lea esi, [students]            ; Pointer to the students array
+
+print_students:
+    mov eax, [esi + student_t.check] ; Load the "check" field
+    test eax, eax                    ; Check if the student is assigned to a course
+    jz unassigned_student            ; If not assigned, jump to unassigned_student
+
+    ; Print student name
+    lea eax, [esi + student_t.name]  ; Load the address of the student's name
+    push eax
+    PRINTF32 `Student: %s\n\x0`
+    add esp, 4
+
+    ; Print course name
+    mov eax, [esi + student_t.id_course] ; Load the course ID
+    lea ebx, [courses]                   ; Pointer to the courses array
+    imul eax, eax, course_t              ; Multiply course ID by the size of course_t
+    add ebx, eax                         ; Add offset to the courses array
+    lea eax, [ebx + course_t.name_course] ; Load the address of the course name
+    push eax
+    PRINTF32 `Course: %s\n\x0`
+    add esp, 4
+    jmp next_student
+
+unassigned_student:
+    ; Print unassigned message
+    push unassigned
+    PRINTF32 `%s\n\x0`
+    add esp, 4
+
+next_student:
+    add esi, student_t                  ; Move to the next student
+    loop print_students                 ; Repeat for all students
+
     leave
     ret
--- labs/lab-06/tasks/find-substring/support/find_substring.asm
+++ labs/lab-06/tasks/find-substring/support/find_substring.asm
@@ -15,7 +15,42 @@ main:
     push ebp
     mov ebp, esp
 
-    ; TODO: Print the start indices for all occurrences of the substring in source_text
+    ; Initialize pointers and counters
+    lea esi, [source_text]       ; Load address of source_text into esi
+    lea edi, [substring]         ; Load address of substring into edi
+    xor ecx, ecx                 ; Clear ecx (used for the current index in source_text)
 
+find_next_occurrence:
+    mov ebx, edi                 ; Load address of substring into ebx
+    mov edx, esi                 ; Load current position in source_text into edx
+
+compare_chars:
+    mov al, [edx]                ; Load current character from source_text
+    mov bl, [ebx]                ; Load current character from substring
+    test al, al                  ; Check if end of source_text is reached
+    jz end_search                ; If zero, end the search
+    test bl, bl                  ; Check if end of substring is reached
+    jz substring_found           ; If zero, substring is found
+    cmp al, bl                   ; Compare characters
+    jne next_char                ; If not equal, move to the next character
+    inc edx                      ; Increment source_text pointer
+    inc ebx                      ; Increment substring pointer
+    jmp compare_chars            ; Continue comparing
+
+next_char:
+    inc esi                      ; Increment source_text pointer
+    inc ecx                      ; Increment index counter
+    jmp find_next_occurrence     ; Restart search from the next character
+
+substring_found:
+    push ecx                     ; Push the current index onto the stack
+    push print_format            ; Push the print format string
+    call printf                  ; Call printf to print the index
+    add esp, 8                   ; Clean up the stack
+    inc esi                      ; Move to the next character in source_text
+    inc ecx                      ; Increment index counter
+    jmp find_next_occurrence     ; Continue searching for the next occurrence
+
+end_search:
     leave
     ret
